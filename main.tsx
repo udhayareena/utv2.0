6239ff248b1c6e5a536a89abd76c4bf1c34154ab
udhaya <utv2.0utv2.0@gmail.com>
2025-04-04 13:16:18 +0530

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css' // Ensure this file exists
 
+
+
+<script type="module" src="/src/main.tsx"></script>
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
